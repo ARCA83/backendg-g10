c9e34100b134e7606cffed5c67e2c50b9dbd810a
ARCA83 <anthonycruz0709@gmail.com>
2022-12-21 21:39:27 -0500

    commit inicial

---
+++ Dia-1/2-inicios-flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+# si el archivo es el archivo principal del proyecto su valor de la varible __name__ ser '__main__' caso contrario sera None(vacio)
+app = Flask(__name__)
+#patron de diseño de software (Singleton)
+
+#un decorador se usa con el '@'sirve para modificar cierto metodo de una clase sin la necesidad de modificar el funcionamiento natural(modificacion parcial) luego de utilizar el decorador se crea una funcion que sera la nueva funcionabilidad de ese metodo.
+@app.route('/')
+def inicio():
+    return 'Hola desde mi servidor de Flask'
+
+#debug = cada vez que modifica mos algun archivo  del proyecto y guardamos, se reiniciara el servidor
+app.run(debug=True)

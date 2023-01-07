b63766af2631479897044239c99ed0ce13e78175
ARCA83 <anthonycruz0709@gmail.com>
2023-01-06 21:15:42 -0500

    final proyecto

--- dia3/flask_con_db.py
+++ dia3/flask_con_db.py
@@ -30,21 +30,19 @@ def gestion_productos():
         cursor.execute("SELECT * FROM productos")
         productos = cursor.fetchall() # LIMIT infinito
         # cursor.fetchone() # LIMIT 1
-        #print(productos)
+        # print(productos)
         # cerra nuestra conexion
         cursor.close()
-        resultado =[]
-
+        resultado = []
         for producto in productos:
-          
-            producto_dic ={
-                'id': producto [0],
-                'nombre': producto [1],
-                'imagen': producto [2],
-                'fecha_vencimiento': producto[3].strftime('%Y-%m-%d'),
+            producto_dic = {
+                'id': producto[0],
+                'nombre': producto[1],
+                'imagen': producto[2],
+                'fecha_vencimiento': producto[3].strftime('%Y-%m-%d'), # %H:%M:%S
                 'precio': producto[4],
                 'disponible': producto[5],
-                'categoria_id': producto[6],
+                'categoria_id': producto[6]
             }
             resultado.append(producto_dic)
             print(producto_dic)
@@ -54,31 +52,105 @@ def gestion_productos():
         }
     elif request.method == 'POST':
         cursor = mysql.connection.cursor()
-        informacion = request.get_json() #un diccionario
+        informacion = request.get_json() # un diccionario
         # %s > convierte el contenido a un string
-        # %f > convierte el contenido en numero flotante
-        # %d > convierte el contenido en nuemor entero
-        cursor.execute("INSERT INTO productos(id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id)VALUES (DEFAULT,'%s', '%s', '%s',%f,%s,%d)"% (
+        # %f > convierte el contenido a un numero flotante
+        # %d > convierte el contenido a un numero entero
+        cursor.execute("INSERT INTO productos (id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id) VALUES (DEFAULT, '%s', '%s', '%s', %f, %s, %d)" % (
             informacion.get('nombre'),
-            informacion.get('imagen'),
+            informacion.get('imagen'), 
             informacion.get('fecha_vencimiento'),
             informacion.get('precio'),
             informacion.get('disponible'),
             informacion.get('categoria_id')
-        ))
-        # indicamos que el guardado sea permanente en al base de datos
+            ))
+        # indicamos que el guardado sea permanente en la base de datos
         mysql.connection.commit()
         cursor.close()
-
-
         
         return {
             'message': 'Producto creado exitosamente'
         }
-@app.route("/producto/<int:id>", methods = ['GET','PUT','DELETE'])
-def gestion_un_producto(id):
-    pass
 
+
+def validar_producto(id):
+    # creamos la conexion
+    conexion = mysql.connection.cursor()
+    # ejecutamos el select con la clausula del id
+    conexion.execute("SELECT * FROM productos WHERE id=%d" % (id))
+    # conexion.execute("SELECT * FROM productos WHERE id="+str(id))
+    # conexion.execute("SELECT * FROM productos WHERE id={}".format(id))
+    # conexion.execute(f"SELECT * FROM productos WHERE id={id}")
+    resultado = conexion.fetchone()
+    conexion.close()
+    print(resultado)
+    return resultado
+
+@app.route("/producto/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
+def gestion_un_producto(id):
+    if request.method == 'GET':
+        resultado = validar_producto(id)
+        if resultado is None:
+            return {
+                'message': 'Producto no encontrado'
+            }
+        else:
+            producto = {
+                    'id': resultado[0],
+                    'nombre':resultado[1],
+                    'imagen':resultado[2],
+                    'fecha_vencimiento':resultado[3].strftime('%Y-%m-%d'),
+                    'precio': resultado[4],
+                    'disponible': resultado[5],
+                    'categoria_id': resultado[6]
+                }
+            
+            return {
+                'content': producto
+            }
+    elif request.method == 'PUT':
+        resultado = validar_producto(id)
+        if resultado is None:
+            return {
+                'message': 'Producto no existe'
+            }
+        else:
+            data=request.get_json()
+            conexion = mysql.connection.cursor()
+            conexion.execute("UPDATE productos SET nombre= %s, precio= %s, fecha_vencimiento= %s, categoria_id=%s, disponible=%s, imagen=%s WHERE id=%s", [
+                data.get('nombre'),
+                data.get('precio'),
+                data.get('fecha_vencimiento'),
+                data.get('categoria_id'),
+                data.get('disponible'),
+                data.get('imagen'),
+                resultado[0] # id del producto
+            ])
+            mysql.connection.commit()
+            conexion.close()
+
+            return {
+                'message': 'Producto actualizado exitosamente'
+            }
+    elif request.method == 'DELETE':
+        #DELETE FROM productos WHERE id =...
+        #no se necesita nada del body
+        #hacer la validacion si el producto no existe
+        resultado = validar_producto(id)
+        if resultado is None:
+            return {
+                'message': 'Producto no existe'
+            }
+        else:
+            conexion = mysql.connection.cursor()
+            # primero eliminar de los almacenes
+            conexion.execute("DELETE FROM productos WHERE id = %s",[id])
+            mysql.connection.commit()
+            conexion.close()
+
+            return {
+                'message': 'Producto eliminado exitosamente'
+            }
 
 # load_dotenv > cargamos todas las variables definidas en el archivo .env como si fueran variables de entorno
 app.run(debug=True, load_dotenv=True)

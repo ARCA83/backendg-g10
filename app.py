0bc650b34119737d42152c024c985502b9b215f7
ARCA83 <anthonycruz0709@gmail.com>
2023-01-11 23:05:22 -0500

    USO DE MIGRACIONES

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from os import environ
 from configuracion import conexion
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 from models.categoria_model import Categoria
+from models.productos_model import Producto
 #aca utilizaremos el archivo .env para agregarlo a las variables de entorno
 load_dotenv()
 
@@ -13,11 +15,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
 #inicializar la aplicacion de SQLAlchemy con nuestra aplicacion de flask
 conexion.init_app(app)
 
+# inicializamos la clase Migrate pasandole nuestra aplicacion de Flask y nuestra conexion a SQLAlchemy
+Migrate(app, conexion)
+
 #este controlador se ejecutara antes del primer REQUEST de nuestro servidor
 @app.before_first_request
 def inicializadora():
     #realizar la creacion de todos los modelos de nuestro proyecto como tablas en la base de datos
-    conexion.create_all()
+    #conexion.create_all()
+    pass
 
 if __name__ == '__main__':
     app.run(debug = True)

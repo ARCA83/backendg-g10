7eaa9206a6928254ceee3ff6590cef051a3254e3
ARCA83 <anthonycruz0709@gmail.com>
2023-01-26 22:58:42 -0500

    metodo delete categorias

--- django_intro/almacen/views.py
+++ django_intro/almacen/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ProductosModel, CategoriasModel
 from .serializers import ProductosSerializer, CategoriasSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 
 def renderHtml(request):
@@ -15,32 +15,90 @@ def buscarProducto(request, producto_id):
 class ProductosView(generics.ListCreateAPIView):
     queryset = ProductosModel.objects.all()
     serializer_class = ProductosSerializer
-#https://www.django-rest-framework.org/api-guide/views/
+
 class CategoriasView(generics.GenericAPIView):
-    queryset = CategoriasModel.objects.all()
     serializer_class = CategoriasSerializer
+    queryset = CategoriasModel.objects.all()
 
     def get(self, request):
         try:
             record = self.get_queryset()
-            serializer= self.get_serializer(record, many=True)  
-            return Response(serializer.data)
-        except:
-            return Response({
-                'message' : 'Internal server error'
-            })
-        
+            serializer = self.get_serializer(record, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                data = {
+                    'message': 'Internal server error',
+                    'error': str(e)
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
     def post(self, request):
         try:
             categoria = self.get_serializer(data=request.data)
             if categoria.is_valid():
-                categoria.save( )
-                print(categoria.data)
-                return Response (categoria.data)
+                categoria.save()
+                return Response(categoria.data, status=status.HTTP_201_CREATED)
+
+            error = 'Faltan campos'
+            for campo in categoria.errors:
+                error = error + ' ' + campo + ', '
             return Response({
-                'message':'Transaccion exitosa'
+                'message': error
             })
-        except:
+        except Exception as e:
             return Response({
-                'message' : 'Internal server error'
+                'message': 'Internal server error',
+                'error': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ActualizarCategoriasView(generics.GenericAPIView):
+    queryset = CategoriasModel.objects.all()
+    serializer_class = CategoriasSerializer
+
+    def get(self, request, categoria_id):
+        try:
+            record = self.get_queryset().get(id=categoria_id)
+            serializer = self.get_serializer(record)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                'message': 'Internal server error',
+                'error': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, categoria_id):
+        try:
+            categoria = self.get_queryset().get(id=categoria_id)
+            serializer = self.get_serializer(categoria, data=request.data)
+            if serializer.is_valid():
+                categoria_actualizada = serializer.update(categoria, serializer.validated_data)
+                nuevo_serializador = self.get_serializer(categoria_actualizada)
+                return Response(nuevo_serializador.data, status=status.HTTP_201_CREATED)
+                
+            error = 'Faltan campos'
+            for campo in categoria.errors:
+                error = error + ' ' + campo + ', '
+            return Response({
+                'message': error
             })
+        except Exception as e:
+            return Response({
+                'message': 'Internal server error',
+                'error': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def delete(self, request, categoria_id):
+        try:
+            categoria= self.get_queryset().get(id=categoria_id)
+            serializador = self.get_serializer(categoria)
+            serializador.delete()
+            return Response({
+                'message':'Categoria eliminada correctamente'
+            },status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response ({
+                'message':'Internal server error',
+                'error':str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

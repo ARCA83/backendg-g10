14c7893fda24094326e36996a75bd90fa199a0a8
ARCA83 <anthonycruz0709@gmail.com>
2023-02-15 23:07:25 -0500

    validacion password

--- dia1/index.js
+++ dia1/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { usuarioRouter } from "./routes/usuarios.routes.js";
+import { calendariosRouter } from "./routes/calendarios.routes.js";
 
 const server = express();
 
 server.use(express.json());
 
 server.use(usuarioRouter);
+server.use(calendariosRouter);
 
 const puerto = 3000;
 
